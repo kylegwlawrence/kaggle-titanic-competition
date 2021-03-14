53ffdac6d72c9cbbd054c53e0592b68fba6204b5
Kyle Lawrence <kyle.lawrence@telus.com>
2021-03-14 16:38:29 -0700

    add more models and evaluate using cross validation

--- titanic-competition.py
+++ titanic-competition.py
@@ -1,12 +1,21 @@
 
 # %% Imports
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+#from catboost import CatBoostClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 import seaborn as sn
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
 import sys
-from sklearn.linear_model import LogisticRegression
 
 #get script directory
 here = os.path.dirname(os.path.abspath(__file__))
@@ -160,11 +169,123 @@ train_df = pd.get_dummies(train_df, columns=one_hot_cats, prefix=one_hot_cats)
 # drop some redundant columns. These will be the "base" vars = 1. Also with isalone and iswithparty, base assumption is they are in party of 2
 train_df.drop(labels=['Sex_female','Embarked_S','Pclass_1'],axis=1,inplace=True)
 
-# %% model training
-predictor_vars = train_df[['Age','IsAlone','IsWithParty','PclassxlogFare','Sex_male','Embarked_C','Embarked_Q','Pclass_2','Pclass_3']]
+# %% model training - split data
+X = train_df[['Age','IsAlone','IsWithParty','PclassxlogFare','Sex_male','Embarked_C','Embarked_Q','Pclass_2','Pclass_3']]
+y = train_df['Survived']
+X_train, X_test, Y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=101)
 
-response_var = train_df['Survived']
+#%%1. logistic regression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+print('Logistic regression score = {}'.format(acc_log))
+confusion_matrix(y_test, Y_pred)
 
-#1. logistic regression
+#%% 2. support vector machine
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+print('Support vector machine score = {}'.format(acc_svc))
+confusion_matrix(y_test, Y_pred)
+
+# %% 3. k nearesty neaighbour
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+print('KNN score = {}'.format(acc_knn))
+confusion_matrix(y_test, Y_pred)
+
+# %% 4. gaussian naive bayes
+gaussian = GaussianNB()
+gaussian.fit(X_train, Y_train)
+Y_pred = gaussian.predict(X_test)
+acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+print('Gaussian Naive Bayes score = {}'.format(acc_gaussian))
+confusion_matrix(y_test, Y_pred)
+
+# %% 5. perceptron
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+print('Perceptron score = {}'.format(acc_perceptron))
+confusion_matrix(y_test, Y_pred)
+
+# %% 6. linear svc
+linear_svc = LinearSVC()
+linear_svc.fit(X_train, Y_train)
+Y_pred = linear_svc.predict(X_test)
+acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
+print('Linear SVC score = {}'.format(acc_linear_svc))
+confusion_matrix(y_test, Y_pred)
+
+# %% 7. stochastic gradient
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+print('Stochastic gradient score = {}'.format(acc_sgd))
+confusion_matrix(y_test, Y_pred)
+
+# %% 8. Decision tree
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+print('Decision tree score = {}'.format(acc_decision_tree))
+confusion_matrix(y_test, Y_pred)
+
+# %% 9. random forest
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+print('Random forest score = {}'.format(acc_random_forest))
+confusion_matrix(y_test, Y_pred)
+
+# %% put models scores in a dataframe
+models = pd.DataFrame({'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+    'Random Forest', 'Naive Bayes', 'Perceptron', 'Stochastic Gradient Decent',
+    'Linear SVC', 'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log, acc_random_forest, acc_gaussian, acc_perceptron,
+    acc_sgd, acc_linear_svc, acc_decision_tree]})
+
+models.sort_values(by='Score', ascending=False, ignore_index=True)
+
+#%% Create a list which contains classifiers
+
+classifiers = []
+classifiers.append(LogisticRegression())
+classifiers.append(SVC())
+classifiers.append(KNeighborsClassifier(n_neighbors=5))
+classifiers.append(GaussianNB())
+classifiers.append(Perceptron())
+classifiers.append(LinearSVC())
+classifiers.append(SGDClassifier())
+classifiers.append(DecisionTreeClassifier())
+classifiers.append(RandomForestClassifier())
+
+len(classifiers)
+
+# %%Create a list which contains cross validation results for each classifier
+cv_results = []
+for classifier in classifiers:
+    cv_results.append(cross_val_score(classifier, X_train,
+        Y_train, scoring='accuracy', cv=10))
+# %% k-fold cross validation
+cv_mean = []
+cv_std = []
+for cv_result in cv_results:
+    cv_mean.append(cv_result.mean())
+    cv_std.append(cv_result.std())
+    
+cv_res = pd.DataFrame({'Cross Validation Mean': cv_mean, 'Cross Validation Std': cv_std, 'Algorithm': [
+    'Logistic Regression', 'Support Vector Machines', 'KNN', 'Gausian Naive Bayes', 'Perceptron', 'Linear SVC', 'Stochastic Gradient Descent', 'Decision Tree', 'Random Forest']})
+cv_res.sort_values(by='Cross Validation Mean',
+    ascending=False, ignore_index=True)
 
 # %%
