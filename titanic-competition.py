0a5e6aa5a7afb722e2327bf6a905c725025a71ef
Kyle Lawrence <kyle.lawrence@telus.com>
2021-03-13 16:17:11 -0800

    check out various bin sizes for histograms of age and survival rate

--- titanic-competition.py
+++ titanic-competition.py
@@ -4,13 +4,7 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import datetime as dt
-from dateutil.relativedelta import relativedelta
-from sqlalchemy import create_engine
-import sqlalchemy
 import os
-import math
-import time
 import sys
 start_time = time.time()
 
@@ -64,6 +58,43 @@ corrMatrix = train_df[numericals].corr()
 plt.figure(figsize=(5, 5))
 sn.heatmap(corrMatrix, annot=True, linewidths=1)
 plt.show()
+
+# %%bin the ages and plot survival rate
+def histogram_age(cut_num_bins):
+    train_df['bin_age_{}'.format(cut_num_bins)] = pd.cut(train_df['Age'], bins=cut_num_bins)
+    
+    survived = train_df[train_df['Survived'] == 1][['Survived', 'bin_age_{}'.format(cut_num_bins)]
+    ].groupby(['bin_age_{}'.format(cut_num_bins)]).agg({'Survived': 'count'})
+    
+    not_survived = train_df[train_df['Survived'] == 0][['Survived', 'bin_age_{}'.format(cut_num_bins)]
+        ].groupby(['bin_age_{}'.format(cut_num_bins)]).agg({'Survived': 'count'})
+    
+    survive_rate = survived/(not_survived+survived)
+
+    ax = survive_rate[['Survived']].plot(
+        kind='bar', title="Age Histogram Survival rate", figsize=(8, 5), legend=False, fontsize=12)
+    ax.set_xlabel("Age", fontsize=12)
+    ax.set_ylabel(
+        "% survived", fontsize=12)
+    plt.show()
+    train_df.drop(['bin_age_{}'.format(cut_num_bins)],axis=1,inplace=True)
+
+# look at histograms with different bin sizes
+for n in range(3,9):
+    histogram_age(n)
+
+#%%
+exploration_results = """
+    More likely to survive if female than male. This makes sense given the women and childern first strategy upon evacuation.
+    \n
+    First class passengers are more likely to survive than second class passengers and second class passengers are more likely to survive than third class passengers. This makes sense given the class priority upon evacuation.
+    \n 
+    I looked up the definitions of the Embarked codes. C = Cherbourg, Q = Queenstown, S = Southampton. Survival is more likely for passengers from Cherbourg than anywhere else. Survival of passengers from Southampton is least likely. There might be a correlation between embarked location and class.
+    \n
+    Investigating correlation amongst numericals columns, Fare is the most strongly correlated numerical category with Survived. This means the more a passenger paid, the more likely they were to survive. Parch is also positively correlted with survival although weakly. Age and SibSp are both weakly negatively correlated with Survived.
+    \n
+    Histograms of different age bins across survival rate shows that younger passengers are more likely to survive than older passengers in general. The highest survival rate when splitting age across 8 bins is 0-10 years old (~60%) and the lowest survival rate is the last bin, 70 to 80 years old (~20%)
+"""
 
 
 
